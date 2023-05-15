e9fb6035d61e101da0cdd22446878f0a6de1f667
Asil Awad <aseelawad8@hotmail.com>
2023-05-15 05:22:36 +0300

    Add Comments

--- app/src/main/java/org/asl/mylibrary/MainActivity.java
+++ app/src/main/java/org/asl/mylibrary/MainActivity.java
@@ -16,9 +16,10 @@ import org.asl.mylibrary.activities.library_activities.FavouriteBooksActivity;
 import org.asl.mylibrary.activities.library_activities.WantToReadActivity;
 import org.asl.mylibrary.utils.Utils;
 
+//Main activity file that contain the logic for the main screen of the app
 public class MainActivity extends AppCompatActivity {
-    
-    
+
+    // Declare buttons used on the main screen
     private Button btnAllBooks,
             btnAlreadyRead,
             btnWantToRead,
@@ -28,10 +29,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //sets the layout of the activity to the activity_main
         setContentView(R.layout.activity_main);
+
+        // Initialize the buttons on the main screen
         initViews();
 
-
+        // Set click listeners for each button
         btnAllBooks.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,6 +82,9 @@ public class MainActivity extends AppCompatActivity {
         Utils.getInstance(this);
     }
 
+
+
+//    The initViews method initializes the button variables by finding them in the layout using their IDs.
     private void initViews() {
         btnAllBooks = findViewById(R.id.btnAllBooks);
         btnAlreadyRead = findViewById(R.id.btnAlreadyRead);
